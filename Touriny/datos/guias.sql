f0991b9673cc54ed546e51900eb0ea9c62237523
fernandocutire <fernandocutire@gmail.com>
2021-07-08 18:30:27 -0500

    NEW: Guias actualizados

--- Touriny/datos/guias.sql
+++ Touriny/datos/guias.sql
@@ -1,1 +1,1 @@
-INSERT INTO guia VALUES (1, '8-456-875', 'Fernando', 'Gabriel', 'Diaz', 'Messi', 'fernando.diaz@outlook.com', '6870-7239', 20 , 158 );
+INSERT INTO guia VALUES (1, '8-456-875', 'Fernando', 'Diaz', 'fernando.diaz@outlook.com', '6870-7239', 20 , 'Panamá' );
