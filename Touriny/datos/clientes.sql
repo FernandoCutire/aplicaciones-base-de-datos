948fabd73c191f799d335f0945de768d3a297316
fernandocutire <fernandocutire@gmail.com>
2021-07-08 18:43:39 -0500

    NEW: tours y dificultad añadidos

--- Touriny/datos/clientes.sql
+++ Touriny/datos/clientes.sql
@@ -1,3 +1,2 @@
-INSERT INTO cliente VALUES (8977, 'E-862826', 'Boris', 'Nelson', 'Flores', 'Mendoza', 'BORIS9067@MAIL.COM', '6879-2546', '21', 158 );
-
+INSERT INTO cliente VALUES (8977, 'E-862826', 'Boris', 'Nelson', 'Flores', 'Mendoza', 'BORIS9067@MAIL.COM', '6879-2546', 22, 4, 158 )
 
---
+++ Touriny/datos/tours.sql
@@ -0,0 +1,2 @@
+INSERT INTO dificultad VALUES (1, 'Bocas del Toro', '15-jun-2021', '30-jun-2021', 20, 'Mira las mejores playas en el menor tiempo', 100, 25, 1, 1, 1);
+
