6358d176dfe5481dd5604aeab8f1d407b6d1e251
fernandocutire <fernandocutire@gmail.com>
2021-07-10 18:21:41 -0500

    PROGRESS: Vistas - dias y guias frecuentados inicio

--- Touriny/vistas/2_dias_habituales.sql
+++ Touriny/vistas/2_dias_habituales.sql
@@ -1,6 +1,12 @@
 SELECT
-    SUM(CASE WHEN extract(day_of_week from fecha_booking) = 1 ) AS "Primer Cuatrimestre",
-    SUM(CASE WHEN extract(day_of_week from fecha_booking) = 2) AS "Segundo Cuatrimestre",
-    SUM(CASE WHEN extract(day_of_week from fecha_booking) = 3) AS "Tercer Cuatrimestre",
-    SUM(CASE WHEN extract(day_of_week from fecha_booking) = 4) AS "Cuarto Cuatrimestre"
+    SUM(CASE WHEN extract(day_of_week from fecha_booking) = 'Monday' ) AS "Lunes",
+    SUM(CASE WHEN extract(day_of_week from fecha_booking) = 'Tuesday') AS "Martes",
+    SUM(CASE WHEN extract(day_of_week from fecha_booking) = 'Wednesday') AS "Miércoles",
+    SUM(CASE WHEN extract(day_of_week from fecha_booking) = 'Thursday') AS "Jueves"
+    SUM(CASE WHEN extract(day_of_week from fecha_booking) = 'Friday') AS "Viernes"
  FROM BOOKING;
+
+
+select extract <day_of_week from fecha_booking> as dias from BOOKING;
+
+select EXTRACT("TimeStamp", DAY_OF_WEEK)
---
+++ Touriny/vistas/7_guias_frecuentados.sql
@@ -0,0 +1,2 @@
+Select g.nombre
+FROM Guias g;
