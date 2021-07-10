48d7c378928723cc075e00ab8ec0e42d35a40e8f
Fernando Cutire ubuntu <fernandocutire@gmail.com>
2021-07-10 16:43:55 -0500

    NEW: Guias Frecuentados

---
+++ Touriny/vistas/1_clientes_periodo.sql
@@ -0,0 +1,8 @@
+/* Ayudar con eso*/
+/* Cuatrimestres */
+
+SELECT p.pais_nombre, COUNT(c.id_cliente)
+FROM PAIS p
+INNER JOIN Cliente c ON c.cod_pais = p.id_pais
+GROUP BY P.pais_nombre
+ORDER BY COUNT(c.id_cliente) DESC;
---
+++ Touriny/vistas/3_consumidor_nacionalidad.sql
@@ -0,0 +1,8 @@
+/* Cuatrimestres */
+
+
+SELECT p.pais_nombre, COUNT(c.id_cliente)
+FROM PAIS p
+INNER JOIN Cliente c ON c.cod_pais = p.id_pais
+GROUP BY P.pais_nombre
+ORDER BY COUNT(c.id_cliente) DESC;
