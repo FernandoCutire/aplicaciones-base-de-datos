d939ba290fb4e7415cdfb7dbb9f260f5215cd3df
fernandocutire <fernandocutire@gmail.com>
2021-07-10 17:40:06 -0500

    NEW: Carpeta Utils, vistas 1, 6 y 3

--- Touriny/vistas/1_clientes_periodo.sql
+++ Touriny/vistas/1_clientes_periodo.sql
@@ -1,8 +1,26 @@
 /* Ayudar con eso*/
 /* Cuatrimestres */
+SELECT
+    SUM(CASE WHEN extract(quarter from fecha_booking) = 1 ) AS "Primer Cuatrimestre",
+    SUM(CASE WHEN extract(quarter from fecha_booking) = 2) AS "Segundo Cuatrimestre",
+    SUM(CASE WHEN extract(quarter from fecha_booking) = 3) AS "Tercer Cuatrimestre",
+    SUM(CASE WHEN extract(quarter from fecha_booking) = 4) AS "Cuarto Cuatrimestre"
+ FROM booking;
 
-SELECT p.pais_nombre, COUNT(c.id_cliente)
-FROM PAIS p
-INNER JOIN Cliente c ON c.cod_pais = p.id_pais
-GROUP BY P.pais_nombre
-ORDER BY COUNT(c.id_cliente) DESC;
+
+/*
+
+OUTPUT:
+
+1 CUATRIMESTRE
+5
+
+2 CUATRIMESTRE
+5
+
+3 CUATRIMESTRE
+10
+
+4 CUATRIMESTRE
+8
+*/
