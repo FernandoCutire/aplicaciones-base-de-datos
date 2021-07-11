6d162256e50198ca0b5255a739f89e31acd9da09
diaz-alex <gabriel.diaz4@utp.ac.pa>
2021-07-10 19:43:10 -0500

    nacionalidad y edades views

--- Touriny/vistas/3_consumidor_nacionalidad.sql
+++ Touriny/vistas/3_consumidor_nacionalidad.sql
@@ -6,3 +6,11 @@ FROM PAIS p
 INNER JOIN Clientes c ON c.cod_pais = p.id_pais
 GROUP BY P.pais_nombre
 ORDER BY COUNT(c.id_cliente) DESC;
+
+
+CREATE VIEW VIEW1_NACIONALIDADES
+AS SELECT p.pais_nombre, COUNT(c.id_cliente) AS "Cantidad de clientes"
+FROM PAIS p
+INNER JOIN CLIENTES c ON c.cod_pais = p.id_pais
+GROUP BY P.pais_nombre
+ORDER BY "Cantidad de clientes" DESC;
