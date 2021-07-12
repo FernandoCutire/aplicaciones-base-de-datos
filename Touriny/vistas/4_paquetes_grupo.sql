6d7cf743444167b0127725120233549b125cce40
diaz-alex <gabriel.diaz4@utp.ac.pa>
2021-07-11 19:49:34 -0500

    paquete

--- Touriny/vistas/4_paquetes_grupo.sql
+++ Touriny/vistas/4_paquetes_grupo.sql
@@ -1,8 +1,8 @@
 
  CREATE VIEW VIEW4_PAQUETES
 AS SELECT
-    SUM(CASE WHEN cantidad_personas BETWEEN 0 AND 1 THEN 1 ELSE 0 END) AS "Pauete Individual",
-    SUM(CASE WHEN cantidad_personas BETWEEN 1 AND 2 THEN 1 ELSE 0 END) AS "Pauete Duo",
-    SUM(CASE WHEN cantidad_personas BETWEEN 3 AND 4 THEN 1 ELSE 0 END) AS "Pauete Tripe",
+    SUM(CASE WHEN cantidad_personas BETWEEN 0 AND 1 THEN 1 ELSE 0 END) AS "Paquete Individual",
+    SUM(CASE WHEN cantidad_personas BETWEEN 1 AND 2 THEN 1 ELSE 0 END) AS "Paquete Duo",
+    SUM(CASE WHEN cantidad_personas BETWEEN 3 AND 4 THEN 1 ELSE 0 END) AS "Paquete Tripe",
     SUM(CASE WHEN cantidad_personas BETWEEN 5 AND 10 THEN 1 ELSE 0 END) AS "Paquete Familiar"
  FROM booking;
--- Touriny/vistas/main.sql
+++ Touriny/vistas/main.sql
@@ -26,11 +26,11 @@ AS SELECT p.pais_nombre, COUNT(c.id_cliente) AS "Cantidad de clientes"
 
 -- 4 Crear paquetes a partir de la cantidad de personas que suelen reservar en grupo.​
 
- CREATE VIEW VISTA_4_PAQUETES
+ CREATE VIEW VIEW4_PAQUETES
 AS SELECT
-    SUM(CASE WHEN cantidad_personas BETWEEN 0 AND 1 THEN 1 ELSE 0 END) AS "Pauete Individual",
-    SUM(CASE WHEN cantidad_personas BETWEEN 1 AND 2 THEN 1 ELSE 0 END) AS "Pauete Duo",
-    SUM(CASE WHEN cantidad_personas BETWEEN 3 AND 4 THEN 1 ELSE 0 END) AS "Pauete Tripe",
+    SUM(CASE WHEN cantidad_personas BETWEEN 0 AND 1 THEN 1 ELSE 0 END) AS "Paquete Individual",
+    SUM(CASE WHEN cantidad_personas BETWEEN 1 AND 2 THEN 1 ELSE 0 END) AS "Paquete Duo",
+    SUM(CASE WHEN cantidad_personas BETWEEN 3 AND 4 THEN 1 ELSE 0 END) AS "Paquete Tripe",
     SUM(CASE WHEN cantidad_personas BETWEEN 5 AND 10 THEN 1 ELSE 0 END) AS "Paquete Familiar"
  FROM booking;
 
