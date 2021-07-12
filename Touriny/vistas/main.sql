91d52e7c8112d9ec26857627ce2ce321fb5ee1d0
diaz-alex <gabriel.diaz4@utp.ac.pa>
2021-07-11 19:28:28 -0500

    lista de vistas

--- Touriny/vistas/main.sql
+++ Touriny/vistas/main.sql
@@ -1,22 +1,46 @@
--- Conocer los dias más habituales de reserva. 
+-- 1 Consultar cuántos clientes reservan por distintos periodos de tiempo.​
 
-CREATE VIEW DIAS_HABITUALES 
- AS SELECT TO_CHAR(fecha_booking, 'DAY') "Día", COUNT(fecha_booking) "Cuenta" FROM booking
-GROUP BY TO_CHAR(fecha_booking, 'DAY') 
-ORDER BY TO_CHAR(fecha_booking, 'DAY');
+ CREATE VIEW VISTA_1_CUATRIMESTRE 
+ AS SELECT TO_CHAR(fecha_booking, 'Q') as "Cuatrimestre", COUNT(fecha_booking) as "Cont" FROM booking
+GROUP BY TO_CHAR(fecha_booking, 'Q') 
+ORDER BY TO_CHAR(fecha_booking, 'Q');
 
--- Identificar al consumidor a partir de la nacionalidad. 
 
-CREATE VIEW VIEW1_NACIONALIDADES
+-- 2 Conocer los dias más habituales de reserva. 
+
+ CREATE VIEW VISTA_2_DIAS_HABITUALES 
+AS SELECT TO_CHAR(fecha_booking, 'DAY') "Día", COUNT(fecha_booking) "Cuenta" FROM booking
+    GROUP BY TO_CHAR(fecha_booking, 'DAY') 
+    ORDER BY TO_CHAR(fecha_booking, 'DAY');
+
+
+-- 3 Identificar al consumidor a partir de la nacionalidad. 
+
+ CREATE VIEW VISTA_3_NACIONALIDADES
 AS SELECT p.pais_nombre, COUNT(c.id_cliente) AS "Cantidad de clientes"
-FROM PAIS p
-INNER JOIN CLIENTES c ON c.cod_pais = p.id_pais
-GROUP BY P.pais_nombre
-ORDER BY "Cantidad de clientes" DESC;
+    FROM PAIS p
+    INNER JOIN CLIENTES c ON c.cod_pais = p.id_pais
+    GROUP BY P.pais_nombre
+    ORDER BY "Cantidad de clientes" DESC;
 
--- Determinar el rango de edades más frecuentes de los clientes. 
 
- CREATE VIEW VIEW1_EDADES
+-- 4 Crear paquetes a partir de la cantidad de personas que suelen reservar en grupo.​
+
+ CREATE VIEW VISTA_4_PAQUETES
+AS SELECT
+    SUM(CASE WHEN cantidad_personas BETWEEN 0 AND 1 THEN 1 ELSE 0 END) AS "Pauete Individual",
+    SUM(CASE WHEN cantidad_personas BETWEEN 1 AND 2 THEN 1 ELSE 0 END) AS "Pauete Duo",
+    SUM(CASE WHEN cantidad_personas BETWEEN 3 AND 4 THEN 1 ELSE 0 END) AS "Pauete Tripe",
+    SUM(CASE WHEN cantidad_personas BETWEEN 5 AND 10 THEN 1 ELSE 0 END) AS "Paquete Familiar"
+ FROM booking;
+
+
+-- 5 Establecer los lugares destinos ofrecidos más frecuentados.​
+
+
+-- 6 Determinar el rango de edades más frecuentes de los clientes. 
+
+ CREATE VIEW VIEW_6_EDADES
 AS SELECT
     SUM(CASE WHEN edad BETWEEN 18 AND 24 THEN 1 ELSE 0 END) AS "18-24 Años",
     SUM(CASE WHEN edad BETWEEN 25 AND 54 THEN 1 ELSE 0 END) AS "25-54 Años",
@@ -24,28 +48,10 @@ AS SELECT
     SUM(CASE WHEN edad >65 THEN 1 ELSE 0 END) AS "65+ Años"
  FROM clientes;
 
--- Crear paquetes a partir de la cantidad de personas que suelen reservar en grupo.​
+-- 7 Comparar la cantidad de tours de cada guía turístico
 
-
-
--- Consultar cuántos clientes reservan por distintos periodos de tiempo.​
-
-CREATE VIEW CUATRIMESTRE 
- AS SELECT TO_CHAR(fecha_booking, 'Q') as "Cuatrimestre", COUNT(fecha_booking) as "Cont" FROM booking
-GROUP BY TO_CHAR(fecha_booking, 'Q') 
-ORDER BY TO_CHAR(fecha_booking, 'Q');
-
-
--- Establecer los lugares destinos ofrecidos más frecuentados.​
-
-
-
-
-
--- Comparar la cantidad de tours de cada guía turístico
-
-CREATE VIEW GUIAS_TURISTICOS AS SELECT g.nombre1 "Nombre del Guia" , COUNT(t.id_guia) "Tours"
+ CREATE VIEW VISTA_7_GUIAS_TURISTICOS AS SELECT g.nombre1 "Nombre del Guia" , COUNT(t.id_guia) "Tours"
 FROM Guias g
-INNER JOIN TOURS t ON g.id_guia = t.id_guia
-GROUP BY g.nombre1
-ORDER BY COUNT(t.id_guia) DESC;
+    INNER JOIN TOURS t ON g.id_guia = t.id_guia
+    GROUP BY g.nombre1
+    ORDER BY COUNT(t.id_guia) DESC;
